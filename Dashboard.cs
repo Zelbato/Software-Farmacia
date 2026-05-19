6372b01126efe8b264a032e942748fbec5411f6f
Heitor Zelbato <heitorzelbato@gmail.com>
2026-05-18 21:39:14 -0300

    Alterando nomenclatura da pasta Dashboard

---
+++ Dashboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Software_Farmacia
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
